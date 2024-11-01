83dc7eacbfdb42ff6016e2367328cdb1c7467459
= <=>
2024-11-01 11:24:14 +0530

    Introduction Completed

---
+++ intro.js
@@ -0,0 +1,35 @@
+//Start Date : Ist Nov 2019
+// 2009 - 2015 Javascript had no most changes.
+// 2015 - 2016 ES2016
+
+//If Javascript wants to be the vital language for the web, it needs to be updated.
+
+
+//Declarative Javascript
+//Declarative JavaScript is an approach where you specify what you want to achieve rather 
+//than detailing how to achieve it. In a declarative style, you focus on the final outcome
+//rather than the specific steps to get there. This contrasts with imperative JavaScript,
+//where you write explicit instructions that describe every step in the process
+
+
+const numbers = [1,2,3,4,5]
+const squares = numbers.map(n => n * 2)
+
+
+//Browsers are constantly updating their engines to support the latest features of JavaScript.
+//This means that older browsers may not support the latest features of JavaScript.
+
+
+/*
+Course Overview
+-Template Strings
+-String padding/trimming
+-Destructuring
+-Array find() / includes()
+-Array flat() / flatMap()
+-Iterators, Generators
+-RegExp improvements
+-async/await
+-asyc* yield await
+
+*/
